349fa3f989ef753c540d13cd69dc40edb8dbd1cc
Mort <balazama171@gmail.com>
2026-05-02 05:33:26 +0300

    Update index.tsx
    get out niggaa

--- src/plugins/fakeDeafen/index.tsx
+++ src/plugins/fakeDeafen/index.tsx
@@ -164,7 +164,7 @@ export default definePlugin({
     name: "FakeDeafen",
     description: "You're deafened but you're not.",
     dependencies: ["PhilsPluginLibrary"],
-    authors: [Devs.desu,Devs.viciouscal],
+    authors: [Devs.viciouscal],
 
     patches: [
         {
